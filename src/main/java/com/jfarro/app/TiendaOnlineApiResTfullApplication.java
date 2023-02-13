c3c7f600ba2c45f142b6bd1cd4e1c683079780ea
Joaquin Farro <joaquin.fq.dev@gmail.com>
2023-02-13 10:49:51 -0500

    Subiendo archivos al repositorio de GIT HUB

---
+++ src/main/java/com/jfarro/app/TiendaOnlineApiResTfullApplication.java
@@ -0,0 +1,13 @@
+package com.jfarro.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaOnlineApiResTfullApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaOnlineApiResTfullApplication.class, args);
+    }
+
+}
